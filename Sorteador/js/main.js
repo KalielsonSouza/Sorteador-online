75977b58117b14c8aab121c890f6b6d489c282cc
Kalielson <kalielson.souza@alunos.ufersa.edu.br>
2021-03-05 10:15:52 -0300

    V2 05/03/2021
    Adição da opção de sortear número em um range mínimo. e máximo.
    Adição de pequena animação na hora do sorteio de palavras.

--- Sorteador/js/main.js
+++ Sorteador/js/main.js
@@ -1,15 +1,36 @@
-function sortear()
+function sortWords()
 {
     var textSort = document.getElementById("textToSort").value;
     var TextResult = document.getElementById("resultText");
     var notificationElement = document.getElementById("alertTextEmpty");
+    lineArray = [];
     if(textSort != "")
     {
+        
     notificationElement.style.display = "none";
-    lineArray = [];
     lineArray = textSort.match(/[^\r\n]+/g);
     var result = Math.floor(Math.random() * ((parseInt(lineArray.length)-1) + 1));
-    TextResult.innerHTML = '<p class="lead" id="resultText"><mark> RESULTADO: '+lineArray[result]+'</mark></p>';
+    var i = 0;
+
+    function slideWordsAnimationLoop()
+    {
+        setTimeout(function() 
+        {   
+            TextResult.innerHTML = '<p class="lead" id="resultText"><mark>'+lineArray[i]+'</mark></p>';
+            i++;                    
+            if (i < lineArray.length) 
+            {          
+                    slideWordsAnimationLoop();            
+            } 
+            else
+            {
+                TextResult.innerHTML = '<p class="lead" id="resultText"><mark> RESULTADO: '+lineArray[result]+'</mark></p>';
+            }                      
+        }, 25)
+    }
+    
+    slideWordsAnimationLoop();
+               
     }
     else
     {
@@ -18,3 +39,44 @@ function sortear()
     
     
 }
+
+function sortNumbers()
+{
+    var minNumber = document.getElementById("minInputNumber").value;
+    var maxNumber = document.getElementById("maxInputNumber").value;
+    var result = getRandomInt(minNumber, maxNumber);
+    var TextResult = document.getElementById("resultTextNum");
+    TextResult.innerHTML = '<p class="lead" id="resultTextNum"><mark>RESULTADO: '+result+'</mark></p>';
+}
+
+function divAlternate(caseNum)
+{
+    ///////////// BTNS RELACIONADOS /////////////
+    var btnAlternate = document.getElementById("alternateDivBtn");
+    ///////////// DIVS RELACIONADOS /////////////
+    var divNumber = document.getElementById("numberSortDiv");
+    var divWords = document.getElementById("wordsSortDiv");
+    ///////////// DIVS RELACIONADOS /////////////
+    switch (parseInt(caseNum)) 
+    {
+        case 1:
+            btnAlternate.setAttribute('onclick',  'divAlternate(2)');
+            btnAlternate.innerText = "SORTEADOR DE PALAVRAS";
+            divWords.style.display = "none";
+            divNumber.style.display = "block";
+          break;
+        case 2:
+            btnAlternate.setAttribute('onclick',  'divAlternate(1)');
+            btnAlternate.innerText = "SORTEADOR DE NUMEROS";
+            divWords.style.display = "block";
+            divNumber.style.display = "none";
+          break;
+      }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
